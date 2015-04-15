5bc2892bf3fab361db785a5e78558b1f1f5d441f
Yasset Perez-Riverol <ypriverol@gmail.com>
2015-04-15 21:40:43 +0100

    small changes

--- src/main/java/uk/ac/ebi/ddi/reader/GeneratePxEbeFiles.java
+++ src/main/java/uk/ac/ebi/ddi/reader/GeneratePxEbeFiles.java
@@ -59,7 +59,7 @@ public class GeneratePxEbeFiles {
 
             int initialGap = loopGap;
 
-            for(int i = 1296; i < endPoint && loopGap > 0; i ++){
+            for(int i = 0; i < endPoint && loopGap > 0; i ++){
 
                 String pxID = (pxPrefix + String.valueOf(i));
 
