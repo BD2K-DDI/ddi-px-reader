b372e1403375c10ee00bbd88b004779ff172c490
¨gaurhari¨ <gaurharidass@gmail.com>
2017-08-29 11:01:17 +0100

    added title function

--- src/main/java/uk/ac/ebi/ddi/px/utils/ReadProperties.java
+++ src/main/java/uk/ac/ebi/ddi/px/utils/ReadProperties.java
@@ -49,7 +49,24 @@ public class ReadProperties {
         return prop.getProperty(key);
     }
 
-
+    public static String toTitleCase(String input) {
+        StringBuilder titleCase = new StringBuilder();
+        boolean nextTitleCase = true;
+        for (char c : input.toCharArray())
+        {
+            if (Character.isSpaceChar(c))
+            {
+                nextTitleCase = true;
+            }
+            else if (nextTitleCase)
+            {
+                c = Character.toTitleCase(c);
+                nextTitleCase = false;
+            }
+            titleCase.append(c);
+        }
+        return titleCase.toString();
+    }
 
 
 }
